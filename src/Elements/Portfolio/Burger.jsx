601a2b0000a2c28f40c14bad795b46c8ac1694eb
DanyloSus <danil.sushko19@gmail.com>
2023-11-07 19:26:29 +0200

    Add burger animation

--- src/Elements/Portfolio/Burger.jsx
+++ src/Elements/Portfolio/Burger.jsx
@@ -1,11 +1,26 @@
 import classes from "./Burger.module.css";
 
+import { IsActiveContext } from "../../Contexts/BurgerContext";
+
+import { useContext } from "react";
+
 const Burger = () => {
+  const burgerCtx = useContext(IsActiveContext);
+
   return (
     <div className={classes.burger}>
-      <div className="top" />
-      <div className="center" />
-      <div className="bottom" />
+      <div
+        className="top"
+        style={burgerCtx.isActive ? { rotate: "45deg", top: "15.5px" } : {}}
+      />
+      <div
+        className="center"
+        style={burgerCtx.isActive ? { transform: "scaleY(0)" } : {}}
+      />
+      <div
+        className="bottom"
+        style={burgerCtx.isActive ? { rotate: "-45deg", bottom: "13px" } : {}}
+      />
     </div>
   );
 };
