d574b1600689b7fdca6426e9d39332bd293e327c
DanyloSus <danil.sushko19@gmail.com>
2023-11-08 19:26:52 +0200

    Update for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import Portfolio from "./Pages/Portfolio";
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-const router = createBrowserRouter([
-  // create router (I thought make more than one site)
-  {
-    path: "/", // main path is Portfolio page
-    element: <Portfolio />,
-  },
-]);
-
 const App = () => {
-  // create App element
-  return <RouterProvider router={router} />; // return Router provider with the router
+  return <Portfolio />;
 };
 
 export default App; // return
